68be1b8d404b7737fe1cdb946c41ade77531d83a
adivanced-ui <adivanced@gmail.com>
2026-03-20 21:56:05 +0300

    Add files via upload

---
+++ asm/read.h
@@ -0,0 +1,64 @@
+uint64_t readfile(FILE* file, uint64_t size, float** equ){
+	uint64_t allocsize;
+	if(size+1 <= 8){
+		allocsize = 8;
+	}else if(size+1 <= 16){
+		allocsize = 16;
+	}else if(size+1 <= 24){
+		allocsize = 24;
+	}else if(size+1 <= 32){
+		allocsize = 32;
+	}else if(size+1 <= 40){
+		allocsize = 40;
+	}else if(size+1 <= 48){
+		allocsize = 48;
+	}
+	(*equ) = (float*)malloc(size*allocsize*4);
+	int ret;
+
+	for(uint64_t i = 0; i < size; i++){
+		for(uint64_t j = 0; j < allocsize; j++){
+			if(j <= size){
+				ret = fscanf(file, "%f", (*equ)+i*allocsize+j);
+				if(ret != 1 && ret != -4 && ret != -1){
+					printf("Not a float value supplied in the file\n");
+					exit(0);
+				}
+			}else{
+				*((*equ)+i*allocsize+j) = 0.0f;
+			}
+		}
+	}
+
+	return allocsize;
+
+}
+
+uint64_t readterm(uint64_t* size, float** equ){
+	uint64_t allocsize;
+	printf("Size: ");
+	scanf("%ld", size);
+	if((*size)+1 <= 8){
+		allocsize = 8;
+	}else if((*size)+1 <= 16){
+		allocsize = 16;
+	}else if((*size)+1 <= 24){
+		allocsize = 24;
+	}
+	(*equ) = (float*)malloc((*size)*allocsize*4);
+	int ret;
+	for(uint64_t i = 0; i < (*size); i++){
+		for(uint64_t j = 0; j < allocsize; j++){
+			if(j <= (*size)){
+				ret = scanf("%f", (*equ)+i*allocsize+j);
+				if(ret != 1 && ret != -4 && ret != -1){
+					printf("Not a float value supplied in the file\n");
+					exit(0);
+				}
+			}else{
+				*((*equ)+i*allocsize+j) = 0.0f;
+			}
+		}
+	}
+	return allocsize;
+}
